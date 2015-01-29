0aa65a3b4244b2090deff2b4deecee2872ee8c20
Maximilian Noethe <maximilian.noethe@udo.edu>
2015-01-29 16:57:15 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='mle',
+      version='0.1',
+      description='High Performance Maximum Likelihood Estimations',
+      url='http://github.com/ibab/root_pandas',
+      author='Igor Babuschkin',
+      author_email='igor@babuschk.in',
+      license='MIT',
+      packages=['mle'],
+      install_requires=[
+          'numpy',
+          'scipy',
+          'theano',
+      ],
+      zip_safe=False)
