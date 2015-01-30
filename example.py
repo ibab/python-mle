af0f49059dc62b5229a25af906e93e1af750eceb
Maximilian Noethe <maximilian.noethe@udo.edu>
2015-01-30 17:35:47 +0100

    add example.py

---
+++ example.py
@@ -0,0 +1,31 @@
+from numpy.random import normal
+from numpy import linspace, float32, append
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+from time import clock
+
+from mle import Normal, var, par, Mix2
+
+x = var("x")
+mu1 = par("mu1")
+sigma1 = par("sigma1")
+
+model = Normal(x, mu1, sigma1)
+
+data = normal(3, 5, int(1e8))
+data = data.astype(float32)
+
+start = clock()
+result = model.fit({"x":data}, {"mu1":2, "sigma1":4})
+print("Fit took {:5.1f}".format(clock()-start))
+
+print(result)
+
+mu = result["x"]["mu1"]
+sigma = result["x"]["sigma1"]
+
+px = linspace(-22, 28, 1000).astype(float32)
+
+plt.plot(px, norm.pdf(px, mu, sigma))
+plt.hist(data, 50, histtype="step", normed=True)
+plt.show()
